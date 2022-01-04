be3ce3a8e8e2bb452ef33be021d6d49f80ef67fb
Vincent Almeroth <VAlmeroth@dev-10.com>
2022-01-04 09:43:53 -0600

    Activities class

---
+++ src/main/java/Models/Activities.java
@@ -0,0 +1,80 @@
+package Models;
+
+import java.time.LocalTime;
+import java.time.LocalDate;
+
+public class Activities {
+
+    private int activityId;
+    private String activityName;
+    private String description;
+    private String location;
+    private LocalDate date;
+    private LocalTime meetTime;
+    private int min;
+    private int max;
+
+    public int getActivityId() {
+        return activityId;
+    }
+
+    public void setActivityId(int activityId) {
+        this.activityId = activityId;
+    }
+
+    public String getActivityName() {
+        return activityName;
+    }
+
+    public void setActivityName(String activityName) {
+        this.activityName = activityName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getMeetTime() {
+        return meetTime;
+    }
+
+    public void setMeetTime(LocalTime meetTime) {
+        this.meetTime = meetTime;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+}
