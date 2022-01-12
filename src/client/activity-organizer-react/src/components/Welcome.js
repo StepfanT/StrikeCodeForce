4413f29be7b8acda3c053b242d6d43a9552aa5fe
Muhammed <mwaggeh22@gmail.com>
2022-01-12 16:34:41 -0500

    Update-Muhammed-16 -Some clean up front end -Fixed backend issues

---
+++ src/client/activity-organizer-react/src/components/Welcome.js
@@ -0,0 +1,17 @@
+import '../App.css';
+
+
+export default function Welcome() {
+
+    return (
+        <>
+            <style>{"table{border:1px solid black;}"}
+            </style>
+            <div>
+                <h2 className="my-4"> Welcome </h2>
+                <p>So you've found us. Great, the next step is to log in to join millions of users in the group</p>
+            </div>
+        </>
+    );
+
+}
